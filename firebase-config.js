fbf447db5c808512e90817593cfff0c517898b51
matthiasmannx <matthiasmann175@gmail.com>
2026-09-19 01:59:52 +0200

    Voeg firebase-config.js toe

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Plak hier de configuratie van je Firebase-webapp (Firebase Console > Projectinstellingen > Je apps > SDK-configuratie).
+// Deze waarden zijn geen geheim: de toegang wordt geregeld door de Firestore-regels (firestore.rules).
+// Zolang apiKey met "VUL" begint, werkt de pagina alleen lokaal, zonder inloggen.
+window.VOLHOUDEN_FIREBASE = {
+  apiKey: "VUL-IN",
+  authDomain: "VUL-IN.firebaseapp.com",
+  projectId: "VUL-IN",
+  storageBucket: "VUL-IN.appspot.com",
+  messagingSenderId: "VUL-IN",
+  appId: "VUL-IN"
+};
