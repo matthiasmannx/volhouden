1c2d232d010b5882a6da4a363e645cd32abcfb9d
matthiasmannx <matthiasmann175@gmail.com>
2026-09-19 02:08:00 +0200

    Firebase-configuratie ingevuld

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
-// Plak hier de configuratie van je Firebase-webapp (Firebase Console > Projectinstellingen > Je apps > SDK-configuratie).
+// Configuratie van de Firebase-webapp "volhouden" (project volhouden-ab179).
 // Deze waarden zijn geen geheim: de toegang wordt geregeld door de Firestore-regels (firestore.rules).
-// Zolang apiKey met "VUL" begint, werkt de pagina alleen lokaal, zonder inloggen.
 window.VOLHOUDEN_FIREBASE = {
-  apiKey: "VUL-IN",
-  authDomain: "VUL-IN.firebaseapp.com",
-  projectId: "VUL-IN",
-  storageBucket: "VUL-IN.appspot.com",
-  messagingSenderId: "VUL-IN",
-  appId: "VUL-IN"
+  apiKey: "AIzaSyBuQzELCMI02HYZSWTKuMxXipDllqXSfVc",
+  authDomain: "volhouden-ab179.firebaseapp.com",
+  projectId: "volhouden-ab179",
+  storageBucket: "volhouden-ab179.firebasestorage.app",
+  messagingSenderId: "9890953045",
+  appId: "1:9890953045:web:708b77b569a96b3ccae1dc"
 };
